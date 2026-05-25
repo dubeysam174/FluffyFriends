a3c9bb775fc616e9d9de9026f17e036daeb7b985
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-05-26 01:08:47 +0530

    wrote script to check server is running or not

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express();
+
+const port =5000;
+
+app.get("/",(req,res)=>{
+    res.send("backend running>>>")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`Server running on http://localhost:${port}`)
+})

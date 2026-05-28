da1f51fc151406e008682a7551fc5a6d2bc8223d
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-05-29 00:17:36 +0530

    modifed email feature

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer'
+
+const sendEmail = async ({to, subject,html})=>{
+
+    // create transporter...
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure:false,
+        auth:{
+            user:process.env.EMAIL_USER,
+            pass:process.env.EMAIL_PASS
+        }
+    })
+
+    // send email...
+    await transporter.sendMail({
+        from : `"fluffyFriend" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html
+    })
+}

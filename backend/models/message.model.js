ef20ad16dc1767cde1d61b6b34acf05b91110c54
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-06-03 23:00:19 +0530

    added messagge schema

---
+++ backend/models/message.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    // which conversation...
+    conversation: {
+        type: mongooose.Schema.Types.ObjectId,
+        ref: 'Conversation',
+        required:true
+    },
+
+    // who sent it ...
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+
+    // message Content..
+    text:{
+        type: String,
+        default:''
+
+    },
+
+    // message type...
+    type: {
+        type: String,
+        enum: ['text','image','file'],
+    },
+
+
+    // file/ image url...
+    fileUrl:{
+        type: String,
+        default:''
+    },
+
+    // read status...
+    isRead:{
+        type: Boolean,
+        default: false
+    },
+
+    readAt: { 
+        type: Date
+    }
+},{timestamps:true})
+
+
+export default mongoose.model('Message',messageSchema)

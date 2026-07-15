33a7645e0d07711fd21cb192dbd9edd785b1c552
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-07-16 03:33:00 +0530

    started working on frontend with intialisation of tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>PetCare App....</h1>
+    <div className="min-h-screen bg-red-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+       pet care app emoji🐕
+      </h1>
     </div>
   )
 }

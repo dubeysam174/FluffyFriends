def478e388e71400b2b295aa3f6fa01f3b99255a
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-07-19 02:36:37 +0530

    added authSlice for redux

---
+++ frontend/src/store/slices/authSlice.js
@@ -0,0 +1,47 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+// creating initialstate...
+const initialstate={
+    user:JSON.parse(localStorage.getItem('user')) || null,
+    token: localStorage.getItem('token') || null,
+    loading: false,
+    error: null
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setCredentials: (state,action)=>{
+            const {user,token}=action.payload
+            state.user= user
+            state.token= token
+            localStorage.setItem('token',token)
+            localStorage.setItem('user',JSON.stringify(user))
+
+        },
+        logout: (state)=>{
+            state.user=null
+            state.token=null
+            localStorage.removeItem('token')
+            localStorage.removeItem('user')
+
+        },
+        setLoading: (state,action)=>{
+            state.loading=action.payload
+        },
+        setError: (state,action)=>{
+            state.error=action.payload
+        }
+          
+    }
+})
+
+
+export const {setCredentials,logout,setLoading,setError}=authSlice.actions
+export default authSlice.reducer
+
+export const selectUser= (state)=> state.auth.user
+export const selectToken= (state)=> state.auth.token
+export const selectLoading= (state)=> state.auth.loading
+export const selectError= (state)=> state.auth.error

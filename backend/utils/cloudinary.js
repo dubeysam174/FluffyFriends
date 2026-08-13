2d1627e6f39ac4f8bd19165040d1885cbcce67cc
dubeysam174 <144706165+dubeysam174@users.noreply.github.com>
2026-08-14 04:38:10 +0530

    cloudinary util

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from 'cloudinary'
+
+cloudinary.config({
+     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
+export const uploadCloudinary = async(filePath,folder='fluffyfriends')=>{
+    try {
+        const result = await cloudinary.uploader.upload(filePath,{
+            folder:folder,
+            resource_type:'auto',
+        })
+        return {
+            url: result.secure_url,
+            publicId: result.public_id,
+        }
+    } catch (error) {
+        console.error('cloudinary upload error:',error);
+        throw error;
+    }
+}
+
+export const deleteFromCloudinary= async(publicId)=>{
+   try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.error('Cloudinary delete error:', error);
+    throw error;
+  }
+}
